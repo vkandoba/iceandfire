01c40d83d0710782f12045b2bc37a0f926569113
vkandoba <vkandoba@skbkontur.ru>
2019-05-24 01:10:52 +0500

    приоритет шахтам и нормальный выбор места для постройки

--- Strategy/GrowthStrategy.cs
+++ Strategy/GrowthStrategy.cs
@@ -11,9 +11,22 @@ namespace IceAndFire
 
         public void TrainUnits()
         {
+            if (IceAndFire.game.MyGold >= IceAndFire.MINE_BUILD_COST &&
+                IceAndFire.game.MyGold < IceAndFire.MINE_BUILD_COST + Unit.TrainCosts[1])
+                return;
+
             var placesForTrain = BaseStrategy.PlacesForTrain();
 
-            Strategies.Base.TrainSlave(placesForTrain, 20);
+            Strategies.Base.TrainBase(placesForTrain, GetSlaveTrainPlace, 1, 20);
+        }
+
+        private Position GetSlaveTrainPlace(Position[] places)
+        {
+            var cells = places.ToDictionary(p => p, p => p.Area8()
+                .Where(c => !IceAndFire.game.Map[c.X, c.Y].IsOwned &&
+                            !IceAndFire.game.Map[c.X, c.Y].IsWall).Count());
+            var maxCells = cells.Values.Max();
+            return cells.Where(c => c.Value == maxCells).OrderByDescending(c => IceAndFire.game.MyHq.MDistanceTo(c.Key)).FirstOrDefault().Key;
         }
 
         public void ConstructBuildings()
